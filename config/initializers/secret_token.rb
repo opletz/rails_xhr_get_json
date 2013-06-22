31dcb247a3f0c06ec13f9865952d5b1ceea2c117
Owen <your.name@example.com>
2013-06-22 10:44:15 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsXhrGetJson::Application.config.secret_token = 'cade761d5b2932f0e8722b48575a22e5560b4ebda9811e3307154d82c68745d4bece5b4a882f727d7bd82364157f9b507dcd8af0848d6b13c7ef0a428ac6b3e3'
